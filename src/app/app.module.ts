0691655166a9f2c4fef3902c56c9e9dd4e005dd8
nikhilneha <nikhilbankar7@gmail.com>
2023-03-12 19:54:28 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
-import { HeaderComponent } from './header/header.component';
-import { SideNavComponent } from './side-nav/side-nav.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SideNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
